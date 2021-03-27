5ada5392e50f6351db5527ae2a16bc92969eb37a
vkrypto <vkrypto@gmail.com>
2021-03-28 02:39:57 +0530

    finalizing output app-structure: (rendering-template structure )

--- conf/project_template/settings/__init__.py
+++ conf/project_template/settings/__init__.py
@@ -9,9 +9,9 @@ include(
     'development/*.py',
 
     # include in app settings.
-    optional('../*/settings/default.py'),
-    optional('../*/settings/production.py'),
-    optional('../*/settings/development.py'),
+    optional('../apps/*/settings/default.py'),
+    optional('../apps/*/settings/production.py'),
+    optional('../apps/*/settings/development.py'),
     
     # temp settings.
     optional('local_settings.py')
