ce353f3fbb2af0ef320d6ef1f11fd2416f7944c9
vkrypto <vkrypto@gmail.com>
2021-04-11 03:28:24 +0530

    Auto syncing

--- conf/account_template/settings/others/all_auth.py
+++ conf/account_template/settings/others/all_auth.py
@@ -34,17 +34,13 @@ ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
 
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-ACCOUNT_USERNAME_REQUIRED = False
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
@@ -52,6 +48,15 @@ SOCIALACCOUNT_PROVIDERS = {
         # For each OAuth based provider, either add a ``SocialApp``
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
+        'METHOD': 'oauth2',
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'SCOPE': [
+            'profile',
+            'email',
+            'openid',
+        ],
         'APP1': {
             'client_id': '646567882445-de9h1nnil7891t18g1p3qcnp8hh69kte.apps.googleusercontent.com',
             'secret': 'gyaFu8VsnSmGCT3s-aZM-2Yp',
@@ -62,6 +67,23 @@ SOCIALACCOUNT_PROVIDERS = {
         # For each OAuth based provider, either add a ``SocialApp``
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
+        'METHOD': 'oauth2',
+        'SCOPE': [
+            'email',
+            'public_profile',
+            'user_friends'
+        ],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': lambda request: 'en_US',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.8',
         'APP2': {
             'client_id': '1234',
             'secret': '456',
